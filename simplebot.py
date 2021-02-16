870ccbde5a9c8035f8744238c21a06a0922496be
JCamyre <JWcamry03@gmail.com>
2021-02-16 12:40:33 -0800

    Added abstracted function for finding guilds.

--- simplebot.py
+++ simplebot.py
@@ -11,14 +11,14 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-	for guild in client.guilds:
-		if guild.name == GUILD: # This is the current guild/server the bot is in
-			break
+	# Pass in a function to search through the guilds the bot is in
+	guild = discord.utils.get(client.guilds, name=GUILD) # This is the current guild/server the bot is in
+ 
 	# This is for developers to see what server our bot is connected to via the console
 	print(f'{client.user} has connected to the following guild:\n'
 		f'{guild.name}(id: {guild.id})')
 
-	members = guild.members
-	
+	members = "\n - ".join(sorted([member.name for member in guild.members]))
+	print(f'All members in alphabetical order:\n - {members}')
 
 client.run(TOKEN)
