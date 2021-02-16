3314c79d05109126eb6d312fff1eeae32d50484c
JCamyre <JWcamry03@gmail.com>
2021-02-16 12:23:02 -0800

    Outputs the server name and id that the bot is currently connected to.

--- simplebot.py
+++ simplebot.py
@@ -5,11 +5,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-	print(f'{client.user} has connected to Discord!')
+	for guild in client.guilds:
+		if guild.name == GUILD: # This is the current guild/server the bot is in
+			break
+	# This is for developers to see what server our bot is connected to via the console
+	print(f'{client.user} has connected to the following guild:\n'
+		f'{guild.name}(id: {guild.id})')
+
+	members = guild.members
+	
 
 client.run(TOKEN)
